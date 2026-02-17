26d4690fe63b77aa7a4e667cbd20fee03a6aedab
Nathália Filemon Ferreira da Silva <nathaliaffs1@gmail.com>
2026-02-17 17:35:44 -0300

    Refactor number guessing game functions and variables

--- app.js
+++ app.js
@@ -1,94 +1,85 @@
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Jogo do número secreto';
+let list = []; 
+let limit = 30;
+let numSecret = generateNum();
+let attempts = 1;
 
-//let paragrafo = document.querySelector('p');   - modo classico
-//paragrafo.innerHTML = c;
-
-let lista = []; // lista vazia
-let limite = 30;
-let numSecreto = gerarNum();
-let tentativas = 1;
-
-function exibirTexto(tag, texto){  // oq está em () é um parametro
-    let campo = document.querySelector(tag);
-    campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2}); // junto com a linha 7 do html, esse comando permite que demos uma voz para nosso site
+function showText(tag, text){
+    let field = document.querySelector(tag);
+    field.innerHTML = text;
+    responsiveVoice.speak(text, 'US English Female', {rate:1.2}); 
 }
-function exibirReiniciar() {
-    exibirTexto('h1','Jogo do número secreto'); // criando uma função para o código a cima
-exibirTexto('p',`Escolha um número entre 1 e ${limite}`); // modo simplificado
+function showRestart() {
+    showText('h1','The secret number game');
+showText('p',`Choose a number between 1 and ${limit}`);
 }
 
-exibirReiniciar();
+showRestart();
 
-function verificarChute() {
-    let inputElem = document.querySelector('input'); // seleciona o input
-    let chuteStr = inputElem.value; // pega o valor digitado no input
-    let chute = parseInt(chuteStr, 10);
+function verificarGuess() {
+    let inputElem = document.querySelector('input');
+    let guessStr = inputElem.value;
+    let guess = parseInt(guessStr, 10);
 
-    // validação: vazio
-    if(!chuteStr){ // === igualdade restrita
-        exibirTexto('p', `Valor vazio, por favor digite um número entre 1 e ${limite}`);
+    if(!guessStr){
+        showText('p', `Empty value, please enter a number between 1 and ${limit}`);
         return;
     }
-    // converte e valida número
-    if(chute > limite){
-        exibirTexto('p', `Número maior do que esperado, por favor digite um número inteiro entre 1 e ${limite}`);
-        limparcampo();
+  
+    if(guess > limit){
+        showText('p', `The number is too high. Please enter an integer between 1 and ${limit}`);
+        cleanfield();
         return;
     }
     // validação: decimal ou negativo
-    if (!/^[0-9]+$/.test(chuteStr)) {
-        exibirTexto('p', 'Valor inválido, por favor digite um valor inteiro positivo');
-        limparcampo();
+    if (!/^[0-9]+$/.test(guessStr)) {
+        showText('p', 'Invalid value. Please enter a positive integer');
+        cleanfield();
         return;
     }
 
-    if(chute === numSecreto){
-        exibirTexto('h1', 'Acertou!!');
-        let palavra = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let msgTentativas = `você descobriu o número secreto com ${tentativas} ${palavra}!!`;
-        exibirTexto('p',msgTentativas);
-        document.getElementById('reiniciar').removeAttribute('disabled'); //o atributo disabled do html foi desativado
+    if(guess === numSecret){
+        showText('h1', 'Correct!!');
+        let word = attempts > 1 ? 'attempts' : 'attempt';
+        let msgattempts = `You guessed the secret number in ${attempts} ${word}!!`;
+        showText('p',msgattempts);
+        document.getElementById('restart').removeAttribute('disabled');
     } else {
-        if(chute > numSecreto){
-            exibirTexto('p','O número secreto é menor');
+        if(guess > numSecret){
+            showText('p','The secret number is lower');
         } else {
-            exibirTexto('p', 'O número secreto é maior');
+            showText('p', 'The secret number is higher');
         }
-        tentativas++;
-        limparcampo();
+        attempts++;
+        cleanfield();
     }
 }
 
-function gerarNum() {
-    let numEscolhido = parseInt(Math.random()*limite+1); // return - retorna um resultado quando a função é chamada
-    let quantidade = lista.length;
+function generateNum() {
+    let numSelected = parseInt(Math.random()*limit+1);
+    let quantity = list.length;
 
-    if(quantidade == limite){
-        lista = [];
+    if(quantity == limit){
+        list = [];
     }
-    if(lista.includes(numEscolhido)){ // "includes" = verifica se o elemento está na lista
-        return gerarNum (); // não é legal colocar em um grande código uma função que chama outra
+    if(list.includes(numSelected)){
+        return generateNum ();
     }else {
-        lista.push(numEscolhido); //push = adiciona no final da lista
-        console.log(lista);
-        return numEscolhido;
+        list.push(numSelected); 
+        console.log(list);
+        return numSelected;
     }
 }
 
-function limparcampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
+function cleanfield() {
+    guess = document.querySelector('input');
+    guess.value = '';
 }
 
-function reiniciarJogo() {
-    numSecreto = gerarNum();
-    console.log(numSecreto);
-    limparcampo();
-    tentativas = 1;
-    exibirReiniciar();
-    document.getElementById('reiniciar').setAttribute('disabled',true); // o atributo disabled do html foi reativado
-    // exibirTexto('h1','Jogo do número secreto'); 
-    // exibirTexto('p','Escolha um número entre 1 e 10'); - poderia ser assim mas para maior organização vamos criar uma função
+function Restartgame() {
+    numSecret = generateNum();
+    console.log(numSecret);
+    cleanfield();
+    attempts = 1;
+    showRestart();
+    document.getElementById('restart').setAttribute('disabled',true);
 }
